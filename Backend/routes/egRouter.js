5e734680a66f6872dc76c8c69e0a0bf2c8802900
Rohanth R B <rbrohanth179@example.com>
2025-06-24 16:22:56 +0530

    day 8

--- Backend/routes/egRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRoute, postRoute, putRoute, deleteRoute } = require('../controllers/egController');
-router.get('/get', getRoute);
-router.post('/post', postRoute);
-router.put('/put', putRoute);
-router.delete('/delete', deleteRoute);
-module.exports = router;
